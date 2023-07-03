fa32c04860a4251c1a3be103668e3e9288f6c4ae
Antonio-87 <davydenko.87@list.ru>
2023-07-03 22:43:40 +0300

    style_innChat

--- src/js/InnChat.js
+++ src/js/InnChat.js
@@ -3,4 +3,26 @@ export default class InnChat {
   constructor(element) {
     this.#element = element;
   }
+
+  markup() {
+    return `
+      <aside class="owners">
+        <ul class="owners-list">
+            <li class="owner you"><div class="check"></div>You</li>
+            <li class="owner"><div class="check"></div>Sara</li>
+            <li class="owner"><div class="check"></div>Mira</li>
+            <li class="owner"><div class="check"></div>Jon</li>
+        </ul>
+      </aside>
+      <div class="chat">
+          <ul class="messages">
+              <li class="message you-owner">
+                  <p class="owner-message you">You, <span class="time">16:08 03.07.2023</span></p><br>
+                  <p class="message-text">Thenks for this chat! It is very important for me!</p>
+              </li>
+          </ul>
+          <textarea class="input-message" placeholder="Type to message here"></textarea>
+      </div>
+    `;
+  }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,13 @@
  * Entry point of app: don't change this
  */
 
-import Controller from "./Controller";
+// import Controller from "./Controller";
 
-const container = document.querySelector(".container");
+// const container = document.querySelector(".container");
 
 document.addEventListener("DOMContentLoaded", () => {
-  const controller = new Controller(container);
-  controller.connectHandler();
+  // const controller = new Controller(container);
+  // controller.connectHandler();
 
-  window.addEventListener("unload", controller.onUnload);
+  // window.addEventListener("unload", controller.onUnload);
 });
